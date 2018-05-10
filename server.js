43e550645a451afb6a345ec8e6db793d4eecaca2
YOJAN <yojan778@gmail.com>
2018-05-10 12:39:54 -0500

    Iniciar Proyecto

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const http = require('http')
+const app = express()
+
+const hostname = '127.0.0.1';
+const PORT = process.env.PORT || 5000
+
+let users = ['oscar', 'juan', 'marcos', 'julieta'];
+let books = [
+    {titulo: 'El señor de los anillos', autor: 'J.R.R. Tolkien'},
+    {titulo: "Cancion de hielo y fuego", autor: 'George RR Martin'}
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ********************************************************************
+// ********************************************************************
+
+// URL raiz de la api
+// http://127.0.0.1:5000
+app.get('/', (req, res) => {
+    res.status(200).send("Welcome to API REST")
+})
+
+// URL para listar todos los usuarios
+// http://127.0.0.1:5000/users
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+// URL para eliminar un usuario
+// http://127.0.0.1:5000/users
+app.post('/users', (req, res) => {
+    let data = req.query;
+    users.push(data.user_name)
+    res.send("New user add")
+})
+
+// URL para actualizar un usuario
+// http://127.0.0.1:5000/users/1
+app.patch('/users/:id',(req, res) => {
+    let params = req.params;
+    let data = req.query;
+    users[params.id] = data.user_name
+    res.send("User update")
+})
+
+// URL para eliminar un usuario
+// http://127.0.0.1:5000/users/1
+app.delete('/users/:id',(req, res) => {
+    let params = req.params;
+    users.splice(params.id, 1);
+    res.send('User delete')
+})
+
+// ********************************************************************
+// ********************************************************************
+
+// Crear y lanzar el servidor
+http.createServer(app).listen(PORT, () => {
+    console.log(`Server running at http://${hostname}:${PORT}/`);
+})
